f46e73316d56b6a9361376c33d7eecccb5b82a42
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-18 16:11:13 -0400

    added params

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAefFzy08JxCa65lYKR_SiyAtI0PuRwHz8';
+
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
