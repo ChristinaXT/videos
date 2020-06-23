9c0786d4548fbea6ad409f2c1689d4489fa28761
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-23 15:03:34 -0400

    searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,8 +10,8 @@ class SearchBar extends React.Component {
    onFormSubmit = event => {
      event.preventDefault();
 
-     
-   }
+     this.props.onFormSubmit(this.state.term);
+   };
 
   render() {
     return (
