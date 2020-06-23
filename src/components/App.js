176891187e8ca09c3089562e3ed970b947738491
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-23 15:15:10 -0400

    text

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
      return (
        <div className="ui container">
           <SearchBar onFormSubmit={this.onTermSubmit} />
+          I have {this.state.videos.length} videos.
        </div>
      );
    }
