b30c5bcf6112d49ee519f94d8348e40aa0097599
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-23 14:57:10 -0400

    added type param

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -7,7 +7,8 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
+    type: 'video',
     maxResults: 5,
-    key: KEY
+    key: `${KEY}`
   }
-});
+})
