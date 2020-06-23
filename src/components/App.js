aee78c5d2c31af221fcde0732bf337361a795d62
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-23 15:12:49 -0400

    setstate

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
    render() {
