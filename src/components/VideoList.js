e4978813a11e1584c898f3966fcfe5cfa774d747
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-26 20:28:46 -0400

    rendered list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem />;
+  });
 
-  return <div>{videos.length}</div>;
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
