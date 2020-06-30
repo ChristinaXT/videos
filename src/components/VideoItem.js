09cfdbcc73d13dcf4d84fa866030a22330f71e7b
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-30 15:01:22 -0400

    added style

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,10 +4,9 @@ const VideoItem = ({ video }) => {
   return (
     <div className="item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
-      <div>
-         {video.snippet.title}
+      <div className="content">
+         <div className="header">{video.snippet.title}</div>
       </div>
-      
     </div>
   );
 };
