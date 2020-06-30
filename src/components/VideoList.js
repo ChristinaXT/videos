0e9b6c95739a973728888964cb8be81492c9a4be
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-30 16:14:58 -0400

    added to videolist

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem />;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
