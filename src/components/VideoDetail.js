c9d357c064409e6bf66b34ce047a7311f8ee34c8
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-02 17:10:50 -0400

    added src

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,10 +5,12 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <div>
       <div className="ui embed">
-        <iframe src={} />
+        <iframe src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
