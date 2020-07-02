92c1de8795fc432ad2575b9322100862c15844ad
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-02 18:37:57 -0400

    added response

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,10 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items
+      selectedVideo: respose.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
