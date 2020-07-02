42c2b4f0b67fc4557fa94334abcb647e213dd497
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-02 15:23:41 -0400

    added descrip

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,6 +9,7 @@ const VideoDetail = ({ video }) => {
     <div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
       </div>
     </div>
   );
