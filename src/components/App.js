1931565a7587ccc8cb2418c84b000e6178fe0fba
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-02 14:00:45 -0400

    added video select

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-     console.log('From the App!', video);
+     this.setState({ selectedVideo: video });
   };
 
    render() {
